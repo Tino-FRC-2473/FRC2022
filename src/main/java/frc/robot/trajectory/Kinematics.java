15b0a1d8b31206fc706e5bb7b972de5fd6b0d872
RahulBansal <rahulbansal2004@gmail.com>
2022-01-22 18:01:09 -0800

    redid inverse kinematics calcs

--- src/main/java/frc/robot/trajectory/Kinematics.java
+++ src/main/java/frc/robot/trajectory/Kinematics.java
@@ -38,31 +38,70 @@ public class Kinematics {
 
     }
 
-    public void inversekinematics(double gyroHeading, Point robotPos, Point targetPos) {
+    public static Point inversekinematics(double gyroHeading, Point robotPos, Point targetPos) {
         Point newtargetPos = new Point(targetPos.x - robotPos.x, targetPos.y - robotPos.y);
-        double m_RC = Math.tan(gyroHeading);
+
+        double m_RC = Math.tan(Math.toRadians(gyroHeading));
+        if (gyroHeading == 0) m_RC = Math.tan(Math.toRadians(1));
+        else if (gyroHeading == 180) m_RC = Math.tan(Math.toRadians(179));
+
         Point midpoint = new Point(newtargetPos.x / 2.0, newtargetPos.y / 2.0);
         //need to make sure newTargetPos.x does not equal 0 (or isn't close to 0)
-        double m_RT = newtargetPos.y / newtargetPos.x;
-        //need to make sure m_RT isn't close to m_RC
-        double x_c = (newtargetPos.x * m_RC + newtargetPos.y * m_RC * m_RT) / (2.0 * (m_RC - m_RT));
-        //m_RC cannot be 0
-        double y_c = -x_c / m_RC;
+        double m_RT;
+        if (Math.abs(newtargetPos.x) < 0.01) m_RT = newtargetPos.y / 0.01;
+        else m_RT = newtargetPos.y / newtargetPos.x;
+        
+        if (m_RT != m_RT) System.out.println(m_RT);
+        if (Math.abs(newtargetPos.x) < 0.01) m_RT = newtargetPos.y / 0.01;
+        double x_c;
+        double y_c;
+        //if m_RC - m_RT is close to 0, x_c will have a divide by 0 error
+        if (Math.abs(m_RC - m_RT) < 0.01) {
+            x_c = (newtargetPos.x * m_RC + newtargetPos.y * m_RC * m_RT) / (2.0 * (m_RC - m_RT < 0 ? -1 : 1) * 0.01);
+            //if the heading is one way and the target point is 180 deg
+            //in the other direction, set the center of the circle at ~(0, 0)
+            if (gyroHeading > 270 && gyroHeading < 90 & newtargetPos.x < 0) {
+                y_c = 0;
+            } else if (gyroHeading < 270 && gyroHeading > 90 & newtargetPos.x > 0) {
+                y_c = 0;
+            } else {
+                y_c = -x_c / m_RC;
+            }
+
+        } else {
+            x_c = (newtargetPos.x * m_RC + newtargetPos.y * m_RC * m_RT) / (2.0 * (m_RC - m_RT));
+            y_c = -x_c / m_RC;
+        }
+
         double radius = getMagnitude(x_c, y_c);
+
         double RA = getMagnitude(midpoint.x, midpoint.y);
         double AC = getMagnitude(midpoint.x - x_c, midpoint.y - y_c);
+        
         double alpha = Math.atan2(RA, AC);
+        
         double radius_i = radius - Constants.TRACKWIDTH_IN / 2.0;
         double radius_o = radius + Constants.TRACKWIDTH_IN / 2.0;
+        
         double s_i = 2 * radius_i * alpha;
         double s_o = 2 * radius_o * alpha;
+        
         //powers to set the inner and outer set of wheels
         double p_i = s_i / s_o;
         double p_o = 1;
+        
+        //find out whether the left or right side is the inner/outer set of wheels
+        double targetAngle = Math.atan2(newtargetPos.y, newtargetPos.x);
+        if(targetAngle - gyroHeading > 0 || targetAngle - gyroHeading < -180) {
+            //left side is the inner side
+            return new Point(p_i, p_o);
+        }else {
+            //right side is the inner side
+            return new Point(p_o, p_i);
+        }
 
     }
-
-    private double getMagnitude(double a, double b) {
+    private static double getMagnitude(double a, double b) {
         return Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
     }
 }
